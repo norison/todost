28b8c7f781178e5ee3db1bfd917e4d89a46f996e
Ihor Matiev <ihor.matiev@gmail.com>
2026-02-03 21:35:08 +0100

    add supabase and swiftui projects

---
+++ src/apple/Todost/Todost/TodostApp.swift
@@ -0,0 +1,32 @@
+//
+//  TodostApp.swift
+//  Todost
+//
+//  Created by Ihor Matiev on 03.02.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TodostApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
